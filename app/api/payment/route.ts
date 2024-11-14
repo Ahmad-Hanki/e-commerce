3759d32aba63dc875910c464dc0d9836320284b3
ahmad <itxti909@gmail.com>
2024-11-14 14:05:16 +0300

    tryig

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -1,13 +1,40 @@
 import { NextResponse } from "next/server";
+import crypto from "crypto";
+
+const merchant_key = process.env.PAYTR_MERCHANT_KEY;
+const merchant_salt = process.env.PAYTR_MERCHANT_SALT;
 
 export const POST = async (req: Request) => {
   try {
-    const data = await req.json();
-    console.log(data);
+    // Parse the form data
+    const formData = await req.text();
+    const callback = Object.fromEntries(new URLSearchParams(formData));
 
-    return NextResponse.json({ message: data }, { status: 200 });
+    // Generate hash
+    const paytr_token =
+      callback.merchant_oid +
+      merchant_salt +
+      callback.status +
+      callback.total_amount;
+    const token = crypto
+      .createHmac("sha256", merchant_key!)
+      .update(paytr_token)
+      .digest("base64");
+
+    // Validate the hash
+    if (token !== callback.hash) {
+      throw new Error("PAYTR notification failed: bad hash");
+    }
+
+    if (callback.status === "success") {
+      // Process successful payment
+    } else {
+      // Handle unsuccessful payment
+    }
+
+    return NextResponse.json({ message: "OK" }, { status: 200 });
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ error: error }, { status: 500 });
+    return NextResponse.json({ error }, { status: 500 });
   }
 };
