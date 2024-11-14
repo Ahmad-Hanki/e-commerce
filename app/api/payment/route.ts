ac23538a4fb9dedec5c745ef3096b407be129e76
ahmad <itxti909@gmail.com>
2024-11-14 13:54:05 +0300

    push

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -1,8 +1,13 @@
 import { NextResponse } from "next/server";
 
 export const POST = async (req: Request) => {
-  const data = await req.json();
-  console.log(data);
-  return NextResponse.json({ message: data }, { status: 200 });
-};
+  try {
+    const data = await req.json();
+    console.log(data);
 
+    return NextResponse.json({ message: data }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+};
