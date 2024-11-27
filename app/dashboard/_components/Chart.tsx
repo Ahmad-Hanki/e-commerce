02040fb4d1d17d19fdc2d3a32fcc4b9eff5f02e5
ahmad <itxti909@gmail.com>
2024-11-27 16:44:02 +0300

    changes

--- app/dashboard/_components/Chart.tsx
+++ app/dashboard/_components/Chart.tsx
@@ -41,45 +41,45 @@ const chartConfig = {
 export default function Chart({ data }: ChartProps) {
   return (
     <div className="flex-1 w-full">
-        <Card>
-          <CardHeader>
-            <CardTitle>Toplam Ödeme</CardTitle>
-            <CardDescription>2024 - 2025</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <ChartContainer config={chartConfig}>
-              <BarChart className="w-full" height={250} data={data}>
-                <CartesianGrid vertical={false} />
-                <XAxis
-                  dataKey="month"
-                  tickLine={false}
-                  tickMargin={10}
-                  axisLine={false}
-                  tickFormatter={(value) => value.slice(0, 3)} // Format month abbreviation
-                />
-                <YAxis />
-                <Tooltip
-                  formatter={(value) => [value, chartConfig.totalPayment.label]} // Tooltip only for totalPayment
-                />
-                <Legend
-                  formatter={() => chartConfig.totalPayment.label} // Legend only for totalPayment
-                />
+      <Card>
+        <CardHeader>
+          <CardTitle>Toplam Ödeme</CardTitle>
+          <CardDescription>2024 - 2025</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <ChartContainer config={chartConfig}>
+            <BarChart className="w-full" height={250} data={data}>
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="month"
+                tickLine={false}
+                tickMargin={10}
+                axisLine={false}
+                tickFormatter={(value) => value.slice(0, 3)} // Format month abbreviation
+              />
+              <YAxis />
+              <Tooltip
+                formatter={(value) => [value, chartConfig.totalPayment.label]} // Tooltip only for totalPayment
+              />
+              <Legend
+                formatter={() => chartConfig.totalPayment.label} // Legend only for totalPayment
+              />
 
-                <Bar
-                  dataKey="totalPayment"
-                  fill={chartConfig.totalPayment.color}
-                  radius={[4, 4, 0, 0]}
-                  barSize={45} // Adjust the width of the bar to fit the container better
-                />
-              </BarChart>
-            </ChartContainer>
-          </CardContent>
-          <CardFooter className="flex-col items-start gap-2 text-sm">
-            <div className="leading-none text-muted-foreground">
-              Son 12 aya ait veriler gösteriliyor
-            </div>
-          </CardFooter>
-        </Card>
+              <Bar
+                dataKey="totalPayment"
+                fill={chartConfig.totalPayment.color}
+                radius={[4, 4, 0, 0]}
+                barSize={45} // Adjust the width of the bar to fit the container better
+              />
+            </BarChart>
+          </ChartContainer>
+        </CardContent>
+        <CardFooter className="flex-col items-start gap-2 text-sm">
+          <div className="leading-none text-muted-foreground">
+            Son 12 aya ait veriler gösteriliyor
+          </div>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
